be1b915cce431894eda4ca6c1e3b7ff352cbb3a7
ganeshdarga <118905448+ganeshdarga@users.noreply.github.com>
2024-12-13 18:29:50 +0530

    Update Createticket.js

--- src/UserComponents/Createticket.js
+++ src/UserComponents/Createticket.js
@@ -17,7 +17,7 @@ const Createticket = () => {
 
 
     const SubmitTicket = ()=>{
-        Axios.post("https://crmbackend-1-t10q.onrender.com/api/v1/ticket/create_ticket/",{
+        Axios.post("https://crmbackend-1-9mff.onrender.com/api/v1/ticket/create_ticket/",{
             subject:subject,
             tasktype:tasktype,
             Prioritys:Prioritys,
